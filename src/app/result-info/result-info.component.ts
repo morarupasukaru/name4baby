15aba06598de78b962dd0e34c518fec7d757fab7
morarupasukaru <moraru.pasukaru@gmail.com>
2019-05-12 13:01:28 +0200

    add dynamic

--- src/app/result-info/result-info.component.ts
+++ src/app/result-info/result-info.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultInfoComponent implements OnInit {
 
+  resultCount = 10;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultComponent implements OnInit {
 
+  searching = false;
+
   constructor() { }
 
   ngOnInit() {
