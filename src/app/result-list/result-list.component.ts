036559f8f2d20c8e959b1e0f399d7aef96a604fc
morarupasukaru <moraru.pasukaru@gmail.com>
2019-05-12 11:48:00 +0200

    update

--- src/app/result-list/result-list.component.ts
+++ src/app/result-list/result-list.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultListComponent implements OnInit {
 
+  firstnames = [
+    { firstname: 'Lidia', like: false },
+    { firstname: 'Lydia', like: true },
+    { firstname: 'Elodie', like: true },
+    { firstname: 'Amélie', like: true },
+    { firstname: 'Linda', like: false },
+    { firstname: 'Chloé', like: false },
+    { firstname: 'Lisa', like: true },
+    { firstname: 'Julie', like: true },
+    { firstname: 'Aline', like: false },
+    { firstname: 'Alycia', like: false }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onToggle(firstname) {
+    firstname.like = !firstname.like;
+  }
 }
