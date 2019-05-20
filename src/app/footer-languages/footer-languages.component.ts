e40eeb359c5064a8b72c42de6a587d9449de8d17
morarupasukaru <moraru.pasukaru@gmail.com>
2019-05-20 21:16:37 +0200

    update

--- src/app/footer-languages/footer-languages.component.ts
+++ src/app/footer-languages/footer-languages.component.ts
@@ -10,11 +10,18 @@ export class FooterLanguagesComponent implements OnInit {
 
   current: string;
   localStorageLangKey = 'nowname.language';
+  hrefLink: string;
 
   constructor(private translate: TranslateService) {
     let defaultLanguage = localStorage.getItem(this.localStorageLangKey);
     if (!defaultLanguage) {
       defaultLanguage = 'en';
+    }
+    if (location.href.indexOf('localhost:4200') === -1) {
+      // trick to be able to use language link on installed software on github page
+      this.hrefLink = 'https://morarupasukaru.github.io/nowname/#';
+    } else {
+      this.hrefLink = '#';
     }
     this.useLanguage(defaultLanguage);
   }
